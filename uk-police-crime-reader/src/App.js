a1ee9bfa0b04b0c4bc478743741265a9da59e90b
Mateusz Kopij <sagasu@gmail.com>
2020-09-24 18:44:59 +0100

    getting historical crime per street

--- uk-police-crime-reader/src/App.js
+++ uk-police-crime-reader/src/App.js
@@ -7,8 +7,13 @@ const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function App() {
   let dataUrl = "https://data.police.uk/api/metropolitan/neighbourhoods";
+  const lat = 51.464200;
+  const lng = -0.014957;
+  let date = new Date();
+  let twoDigitMonth = ("0" + (new Date(date.getMonth() - 3).getMonth() + 1)).slice(-2);
   //lng and lat for Lewisham below
   // dataUrl = "https://data.police.uk/api/crimes-street/all-crime?lat=51.4415&lng=0.0117&date=2020-09";
+  dataUrl = `https://data.police.uk/api/stops-street?lat=${lat}&lng=${lng}&date=${date.getFullYear()}-${twoDigitMonth}`;
   const {data, error} = useSWR(dataUrl, fetcher);
   
   if(error){
