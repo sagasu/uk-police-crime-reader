11ec74e017bebb0818753cee17b2575c609955a5
Mateusz Kopij <sagasu@gmail.com>
2020-09-24 20:34:26 +0100

    creating a button for each ethnicity so later on we can filter by it

--- uk-police-crime-reader/src/App.js
+++ uk-police-crime-reader/src/App.js
@@ -6,6 +6,12 @@ import useSWR, {SWRConfig} from 'swr';
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function App() {
+  return <SWRConfig value={{fetcher}}>
+    <Crimes />
+  </SWRConfig>
+}
+
+function Crimes(){
   let dataUrl = "https://data.police.uk/api/metropolitan/neighbourhoods";
   const lat = 51.464200;
   const lng = -0.014957;
@@ -14,7 +20,7 @@ export default function App() {
   //lng and lat for Lewisham below
   // dataUrl = "https://data.police.uk/api/crimes-street/all-crime?lat=51.4415&lng=0.0117&date=2020-09";
   dataUrl = `https://data.police.uk/api/stops-street?lat=${lat}&lng=${lng}&date=${date.getFullYear()}-${twoDigitMonth}`;
-  const {data, error} = useSWR(dataUrl, fetcher);
+  const {data, error} = useSWR(dataUrl);
   
   if(error){
     return <div>Error...</div>;
@@ -25,7 +31,22 @@ export default function App() {
   }
   console.log(data, error);
 
-  return<pre>{JSON.stringify(data, null, 2)}</pre>;
-
+  //it is interesting
+  //map is doing an extraction of a data
+  //we cast array that is a result of filtering to a Set to have a unique values (distinct)
+  //with ... operator we cast set to array
+  return <DisplayCrimes 
+     crimes={data}
+     ethnicities={[...new Set(data.map(crime=> crime.self_defined_ethnicity))]}/>;
 }
 
+function DisplayCrimes({crimes, ethnicities}){
+  return(
+    <>
+    {ethnicities.map(ethnicity => (
+      <button key={ethnicity}>{ethnicity}</button>
+    ))}
+      <pre>{JSON.stringify(crimes, null, 2)}</pre>
+    </>
+  );
+}
