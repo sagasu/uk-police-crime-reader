3cf9869bc1ffcb0f04cc02e9929d32cb8e3ebafb
Mateusz Kopij <sagasu@gmail.com>
2020-09-24 18:25:51 +0100

    displaying returned data

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # uk-police-crime-reader
+Reading information from https://data.police.uk feed.   Using SWR (https://swr.vercel.app/) for background fetch.
--- uk-police-crime-reader/src/App.js
+++ uk-police-crime-reader/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import useSWR, {SWRConfig} from 'swr';
 
@@ -8,28 +7,20 @@ const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function App() {
   let dataUrl = "https://data.police.uk/api/metropolitan/neighbourhoods";
+  //lng and lat for Lewisham below
   // dataUrl = "https://data.police.uk/api/crimes-street/all-crime?lat=51.4415&lng=0.0117&date=2020-09";
   const {data, error} = useSWR(dataUrl, fetcher);
   
+  if(error){
+    return <div>Error...</div>;
+  }
+
+  if(!data){
+    return <div>Loading..</div>;
+  }
   console.log(data, error);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return<pre>{JSON.stringify(data, null, 2)}</pre>;
+
 }
 
