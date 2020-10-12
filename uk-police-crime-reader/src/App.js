e6f59253b1831ec30777a1e814ae4c00514aa8cc
Mateusz Kopij <sagasu@gmail.com>
2020-10-12 19:55:04 +0100

    setting lng and lat from input box

--- uk-police-crime-reader/src/App.js
+++ uk-police-crime-reader/src/App.js
@@ -7,6 +7,7 @@ import FilterIcon from '@material-ui/icons/Filter1';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import { ButtonGroup } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 
 const fetcher = (...args) => fetch(...args).then(res => res.json());
 
@@ -17,9 +18,12 @@ export default function App() {
 }
 
 function Crimes(){
+  let [lat, setLat] = React.useState(51.464200);
+  let [lng, setLng] = React.useState(-0.014957);
+
   let dataUrl = "https://data.police.uk/api/metropolitan/neighbourhoods";
-  const lat = 51.464200;
-  const lng = -0.014957;
+  // let lat = 51.464200;
+  // let lng = -0.014957;
   let date = new Date();
   let twoDigitMonth = ("0" + (new Date(date.getMonth() - 3).getMonth() + 1)).slice(-2);
   //lng and lat for Lewisham below
@@ -41,31 +45,39 @@ function Crimes(){
   //we cast array that is a result of filtering to a Set to have a unique values (distinct)
   //with ... operator we cast set to array
   return <DisplayCrimes 
+     position={[lat, setLat, lng, setLng]}
      crimes={data}
      ethnicities={[...new Set(data.filter(crime => crime.officer_defined_ethnicity != null).map(crime=> crime.officer_defined_ethnicity))]}/>;
 }
 
-function DisplayCrimes({crimes, ethnicities}){
+function DisplayCrimes({position, crimes, ethnicities}){
   const [filterEthnicity, setFilterEthnicity] = React.useState(null);
 
   const filterCrimes = filterEthnicity ? crimes.filter(crime => crime.officer_defined_ethnicity === filterEthnicity) : crimes;
 
   return(
     <>
-    <ButtonGroup>
-      {ethnicities.map(ethnicity => (
+    <List>
+      <ListItem>
+        <TextField value={position[0]} onChange={(event) => position[1](event.target.value)} label="latitude" type="number"></TextField>
+        <TextField value={position[2]} onChange={(event) => position[3](event.target.value)} label="longitude" type="number"></TextField>
+      </ListItem>
 
-          <Button endIcon={<FilterIcon />} size="small" variant="contained" color="primary"
-              onClick={() => setFilterEthnicity(ethnicity)}
-              key={ethnicity}>{ethnicity}</Button>
+      <ListItem>
+        <ButtonGroup>
+          {ethnicities.map(ethnicity => (
 
-      ))}
-    
-      {filterEthnicity && <Button startIcon={<RestoreIcon/>} size="small" color="secondary" variant="contained" onClick={() => {
-        setFilterEthnicity(null);
-      }}>reset</Button>}
-    </ButtonGroup>
-    
+              <Button endIcon={<FilterIcon />} size="small" variant="contained" color="primary"
+                  onClick={() => setFilterEthnicity(ethnicity)}
+                  key={ethnicity}>{ethnicity}</Button>
+          ))}
+        
+          {filterEthnicity && <Button startIcon={<RestoreIcon/>} size="small" color="secondary" variant="contained" onClick={() => {
+            setFilterEthnicity(null);
+          }}>reset</Button>}
+        </ButtonGroup>
+      </ListItem>
+    </List>
       <pre>{JSON.stringify(filterCrimes, null, 2)}</pre>
     </>
   );
